f560cd63e017a7176b81ef5738ffde42e9a6ad0f
karl-evalle <karl.evalle@neu.edu.ph>
2021-11-05 22:16:38 +0800

    imageicon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,17 @@
-
 import React from 'react'
-
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-green-700 p-6">
   <div className="flex items-center flex-shrink-0 text-white mr-6">
-    <img className="self-center h-8 mx-3" src="/neulogo.png" />
+    <Image
+        
+        alt ={'announements page'}
+        src={'/neulogo.png'}
+        height='50'
+        width ='50'
+            />
     <span className="font-semibold text-s tracking-tight">NEW ERA UNIVERSITY</span>
   </div>
   <div className="block lg:hidden">

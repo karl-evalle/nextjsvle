6f9f251753fe0ac4948bb8f2cc97c22d8f837037
karl-evalle <karl.evalle@neu.edu.ph>
2021-11-05 15:07:15 +0800

    removedimg

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,6 @@ const Navbar = () => {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-green-700 p-6">
   <div className="flex items-center flex-shrink-0 text-white mr-6">
-    <img className="self-center h-8 mx-3" src="/neulogo.png" />
     <span className="font-semibold text-s tracking-tight">NEW ERA UNIVERSITY</span>
   </div>
   <div className="block lg:hidden">

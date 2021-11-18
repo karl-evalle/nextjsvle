94c603d92bfbed09f75b0c3b5ecfc29f001b57db
karl-evalle <karl.evalle@neu.edu.ph>
2021-11-18 13:08:49 +0800

    mainbranchinvercel

---
+++ prisma/migrations/20211118043209_adduser/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Userprofile` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firtname` VARCHAR(191) NOT NULL,
+    `lastname` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
